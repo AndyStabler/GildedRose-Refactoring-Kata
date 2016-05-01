7e947246da761f67610d5988cb1f2a224ad333cb
Andy Stabler <andy_60@hotmail.co.uk>
2016-05-01 23:21:22 +0100

    Refactor GildedRose
    Extract an ItemDecorator class.
    Create subclasses of the ItemDecorator for each item used in GildedRose.
    Create an ItemFactory that creates the appropriate ItemDecorator based
    on the name.
    Replace the complex logic in update_quality with a call to tick for
    each ItemDecorator instance.
    
    _Massive_ commit - couldn't split this into hunks because of trailing
    whitespace? I guess the original file was written on Windows machine...

--- ruby/gilded_rose.rb
+++ ruby/gilded_rose.rb
@@ -2,54 +2,11 @@ class GildedRose
 
   def initialize(items)
     @items = items
+    @items.map! { |item| ItemFactory.for(item) }
   end
 
   def update_quality()
-    @items.each do |item|
-      if item.name != "Aged Brie" and item.name != "Backstage passes to a TAFKAL80ETC concert"
-        if item.quality > 0
-          if item.name != "Sulfuras, Hand of Ragnaros"
-            item.quality = item.quality - 1
-          end
-        end
-      else
-        if item.quality < 50
-          item.quality = item.quality + 1
-          if item.name == "Backstage passes to a TAFKAL80ETC concert"
-            if item.sell_in < 11
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-            if item.sell_in < 6
-              if item.quality < 50
-                item.quality = item.quality + 1
-              end
-            end
-          end
-        end
-      end
-      if item.name != "Sulfuras, Hand of Ragnaros"
-        item.sell_in = item.sell_in - 1
-      end
-      if item.sell_in < 0
-        if item.name != "Aged Brie"
-          if item.name != "Backstage passes to a TAFKAL80ETC concert"
-            if item.quality > 0
-              if item.name != "Sulfuras, Hand of Ragnaros"
-                item.quality = item.quality - 1
-              end
-            end
-          else
-            item.quality = item.quality - item.quality
-          end
-        else
-          if item.quality < 50
-            item.quality = item.quality + 1
-          end
-        end
-      end
-    end
+    @items.each { |item| item.tick }
   end
 end
 
@@ -64,5 +21,52 @@ class Item
 
   def to_s()
     "#{@name}, #{@sell_in}, #{@quality}"
+  end
+end
+
+class ItemFactory
+
+  def self.for(item)
+    case item.name
+    when "Backstage passes to a TAFKAL80ETC concert"
+      TAFKAL80ETC.new(item)
+    when "Aged Brie"
+      AgedBrie.new(item)
+    when "Sulfuras, Hand of Ragnaros"
+      Sulfuras.new(item)
+    else
+      Unknown.new(item)
+    end
+  end
+end
+
+class ItemDecorator
+  attr_accessor :item
+
+  def initialize(item)
+    @item = item
+  end
+
+  def tick
+  end
+end
+
+class AgedBrie < ItemDecorator
+  def tick
+    item.quality += 1 if item.quality < 50
+    item.sell_in -= 1
+    item.quality += 1 if item.sell_in < 0 && item.quality < 50
+  end
+end
+
+class Sulfuras < ItemDecorator; end
+class Unknown < ItemDecorator; end
+
+class TAFKAL80ETC < ItemDecorator
+  def tick
+    item.quality += 1 if item.quality < 50
+    item.quality += 1 if item.sell_in < 11 && item.quality < 50
+    item.quality += 1 if item.sell_in < 6 && item.quality < 50
+    item.sell_in -= 1
   end
 end
